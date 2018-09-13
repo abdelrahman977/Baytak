881e01a61b332b833462e9a455785c0c2d899da7
Hashim Salem <hashim720@gmail.com>
2018-09-13 13:15:17 +0200

    hi

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,7 +4,10 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { FooterComponent } from './shared/footer/footer.component'
 import { HeaderCustomerComponent } from './shared/header-customer/header-customer.component'
-import { HeaderVisitorComponent } from './shared/header-visitor/header-visitor.component'
+import { HeaderVisitorComponent } from './shared/header-visitor/header-visitor.component';
+import { ListingDetailComponent } from './listing-detail/listing-detail.component';
+import { LandingPageComponent } from './landing-page/landing-page.component';
+import { ListingsResultsComponent } from './listings-results/listings-results.component'
 
 
 
@@ -13,7 +16,10 @@ import { HeaderVisitorComponent } from './shared/header-visitor/header-visitor.c
     AppComponent,
     FooterComponent,
     HeaderCustomerComponent,
-    HeaderVisitorComponent
+    HeaderVisitorComponent,
+    ListingDetailComponent,
+    LandingPageComponent,
+    ListingsResultsComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/listings-results/listings-results.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'listings-results',
+  templateUrl: './listings-results.component.html',
+  styleUrls: ['./listings-results.component.css']
+})
+export class ListingsResultsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
